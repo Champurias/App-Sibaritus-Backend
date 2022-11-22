f45e66bcbba46b0dec19d7ca1a27619f31bc624b
David Cuartas <davidcuba91@gmail.com>
2022-11-22 11:53:23 +0100

    Add routes and include routes error in app

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,10 +1,14 @@
 import morgan from "morgan";
 import express from "express";
+import { generalError, unknownEndpoint } from "./middlewares/error.js";
 
 const app = express();
 
 app.disable("x-powered-by");
 
 app.use(morgan("dev"));
+
+app.use(unknownEndpoint);
+app.use(generalError);
 
 export default app;
