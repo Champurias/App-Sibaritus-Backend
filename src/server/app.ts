f2179cbea78c430d7dba9edf67d6e810118880f8
David Cuartas <davidcuba91@gmail.com>
2022-11-24 16:52:42 +0100

    Add cors white list

--- src/server/app.ts
+++ src/server/app.ts
@@ -2,8 +2,31 @@ import morgan from "morgan";
 import express from "express";
 import { generalError, unknownEndpoint } from "./middlewares/error.js";
 import userRouter from "./routes/userRouter.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+  cors({
+    origin(requestOrigin, callback) {
+      if (
+        !requestOrigin ||
+        [
+          "https://david-cuartas-front-final-202209.netlify.app/",
+          "http://localhost:3000",
+          "http://localhost:4007",
+        ].includes(requestOrigin)
+      ) {
+        callback(null, requestOrigin);
+        return;
+      }
+
+      const error = new Error("No authorization request origin");
+
+      callback(error);
+    },
+  })
+);
 
 app.disable("x-powered-by");
 
