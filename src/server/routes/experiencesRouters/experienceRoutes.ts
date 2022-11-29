fe10dcc6affebfe33eeed299d36b85e526d54f3d
David Cuartas <davidcuba91@gmail.com>
2022-11-29 20:36:39 +0100

    Add supertest to endpoint experienceRoutes

--- src/server/routes/experiencesRouters/experienceRoutes.ts
+++ src/server/routes/experiencesRouters/experienceRoutes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getExperiencies } from "../controllers/experience/experienceController.js";
+import { getExperiencies } from "../../controllers/experience/experienceController.js";
 
 // eslint-disable-next-line new-cap
 export const experienceRouter = express.Router();
