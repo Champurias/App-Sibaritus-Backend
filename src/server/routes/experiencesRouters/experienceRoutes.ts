5c95a7ad5b7d67c226bacdde15644097ef3f0bbd
David Cuartas <davidcuba91@gmail.com>
2022-12-02 13:20:06 +0100

    Add controller and testcontroller

--- src/server/routes/experiencesRouters/experienceRoutes.ts
+++ src/server/routes/experiencesRouters/experienceRoutes.ts
@@ -1,7 +1,11 @@
 import express from "express";
-import { getExperiencies } from "../../controllers/experience/experienceController.js";
+import {
+  deleteExperience,
+  getExperiencies,
+} from "../../controllers/experience/experienceController.js";
 
 // eslint-disable-next-line new-cap
 export const experienceRouter = express.Router();
 
 experienceRouter.get("/list", getExperiencies);
+experienceRouter.delete("/delete/:experienceId", deleteExperience);
